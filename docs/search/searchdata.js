5cffdf56f0ee8c024cfea7a21c06c8a398cf60d2
Tytraman <tytraman@gmail.com>
2022-09-25 14:40:08 +0200

    doc

---
+++ docs/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "c",
+  1: "c"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes"
+};
+
+var indexSectionLabels =
+{
+  0: "Tout",
+  1: "Structures de données"
+};
+
