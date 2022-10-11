1f54d4b142f688d8aaa9a821f78cde2f07461c76
Tytraman <tytraman@gmail.com>
2022-10-11 21:08:06 +0200

    Mis à jour de la documentation

--- docs/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['strutf8_2eh_0',['strutf8.h',['../strutf8_8h.html',1,'']]]
-];
--- docs/search/typedefs_0.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['cake_5flinkedlist_5fstring_5futf8_5fpair_0',['Cake_LinkedList_String_UTF8_Pair',['../strutf8_8h.html#a2d02d2ab9af97b7361bc0e4c1f8b7455',1,'strutf8.h']]],
-  ['cake_5flist_5fstring_5futf8_1',['Cake_List_String_UTF8',['../strutf8_8h.html#aeae31a2c26b84a2e4e167492f86dbbbd',1,'strutf8.h']]],
-  ['cake_5fstring_5futf8_2',['Cake_String_UTF8',['../strutf8_8h.html#ad225949cfabed703f2284878c696200e',1,'strutf8.h']]],
-  ['cake_5fstring_5futf8_5fpair_3',['Cake_String_UTF8_Pair',['../strutf8_8h.html#a5323f1d818b92c3aa3681764095580fc',1,'strutf8.h']]],
-  ['cake_5fstring_5futf8_5freader_4',['Cake_String_UTF8_Reader',['../strutf8_8h.html#a4df195beef077c3c37b75f867610688a',1,'strutf8.h']]]
-];
